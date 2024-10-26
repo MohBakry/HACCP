fd2e4df5dd9aed2a9be3ccdff9c87b35d37fcbea
MohBakry <mbs.tyf@gmail.com>
2024-10-26 19:40:14 +0300

    Home Sections (About+News+Consultaion)

--- app/page.js
+++ app/page.js
@@ -2,11 +2,17 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 import HeroSec from "./(Home)/HeroSec/HeroSec";
+import AboutSec from "./(Home)/AboutSec/AboutSec";
+import NewsSec from "./(Home)/NewsSec/NewsSec";
+import Consultation from "./(Home)/Consultation/Consultation";
 
 export default function Home() {
   return (
     <>
    <HeroSec></HeroSec>
+   <AboutSec></AboutSec>
+   <NewsSec></NewsSec>
+   <Consultation></Consultation>
     </>
   );
 }
