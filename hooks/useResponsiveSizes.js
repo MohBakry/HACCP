3835d18da3dec6c2a386c405615c655c834c922c
sarahBakri <sarabakri96@gmail.com>
2025-04-06 10:31:45 +0200

    last changes

--- hooks/useResponsiveSizes.js
+++ hooks/useResponsiveSizes.js
@@ -2,21 +2,21 @@ import { useState, useEffect } from "react";
 
 const useResponsiveSizes = (breakpoints) => {
   const [size, setSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: window?.innerWidth,
+    height: window?.innerHeight,
   });
 
   const handleResize = () => {
     setSize({
-      width: window.innerWidth,
-      height: window.innerHeight,
+      width: window?.innerWidth,
+      height: window?.innerHeight,
     });
   };
 
   useEffect(() => {
-    window.addEventListener("resize", handleResize);
+    window?.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window?.removeEventListener("resize", handleResize);
     };
   }, []);
 
